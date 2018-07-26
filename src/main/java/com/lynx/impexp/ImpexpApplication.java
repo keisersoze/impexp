16369aeaa0b84fda307f50e2539e9a6430ca6619
Filippo Maganza <filippomaganza@gmail.com>
2018-07-26 14:31:17 +0200

    Openshift

--- src/main/java/com/lynx/impexp/ImpexpApplication.java
+++ src/main/java/com/lynx/impexp/ImpexpApplication.java
@@ -1,12 +1,19 @@
 package com.lynx.impexp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ImpexpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImpexpApplication.class, args);
     }
+
 }
